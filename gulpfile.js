43f72618aaf5e5d89754fd63aa8a8c56d5329d62
Hirotachi <hirotachi@epical.me>
2018-09-11 11:32:29 +0100

    set better workspace install gulp browser-sync gulp-sass

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require("gulp"),
+sass = require("gulp-sass"),
+browserSync = require("browser-sync").create();
+
+
+gulp.task("serve", ["sass"], function() {
+    browserSync.init({
+        notify: false,
+        server:{
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+        return gulp.src("./scss/*.scss")
+            .pipe(sass())
+            .pipe(gulp.dest("./css"))
+            .pipe(browserSync.stream());
+    });
+
+gulp.task('default', ['serve']);
