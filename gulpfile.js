14ffa7138d1747fc87d43b6010858eb61c1458aa
Hirotachi <hirotachi@epical.me>
2018-09-13 23:26:52 +0100

    add components, base, typography, layout styles & edit html for appropriate classes

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,10 @@ gulp.task("serve", ["sass"], function() {
 gulp.task('sass', function() {
         return gulp.src("./scss/*.scss")
             .pipe(sass())
+            .on("error", function(err){
+                console.log(err.toString());
+                this.emit('end');
+            })
             .pipe(gulp.dest("./css"))
             .pipe(browserSync.stream());
     });
